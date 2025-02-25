03a42d023a69f8854193123e910962551f8b2922
KlaraKramer <kramer.k.20@outlook.com>
2025-02-25 10:11:39 +0000

    Further clean up requirements file and start writing unit tests

---
+++ tests/helper_functions_test.py
@@ -0,0 +1,29 @@
+import pytest
+import pandas as pd
+from helper_functions import *
+
+
+# @pytest.fixture
+# def number_a():
+#     return 2
+
+# @pytest.fixture
+# def number_b():
+#     return 3
+
+
+def test_determine_contamination():
+    # Test normal behaviour for more contamination
+    cont_history = [0.2, 0.1, 0.15]
+    output = determine_contamination(cont_history, True)
+    assert output <= 0.5 and output > 0
+
+    # Test normal behaviour for less contamination
+    cont_history = [0.2, 0.3]
+    output = determine_contamination(cont_history, False)
+    assert output <= 0.5 and output > 0
+
+    # Test fallback behaviour for no contamination history
+    cont_history = []
+    output = determine_contamination(cont_history, False)
+    assert output <= 0.5 and output > 0
